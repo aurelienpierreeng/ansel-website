7cf7b943bc54ea261657262d8ad2efa9e0349bf0
Aurélien PIERRE <contact@aurelienpierre.com>
2022-12-06 23:39:41 +0100

    Styling changes

--- themes/ansel/static/js/scripts.js
+++ themes/ansel/static/js/scripts.js
@@ -2,17 +2,19 @@
 // Code adapted from https://www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/
 window.addEventListener('DOMContentLoaded', () => {
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const id = entry.target.getAttribute('id');
       if (entry.intersectionRatio > 0) {
-        document.querySelectorAll('aside nav li').forEach((section) => {
+        document.querySelectorAll('aside nav li a').forEach((section) => {
           section.classList.remove('active');
         });
-        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
       }
-		});
-	});
+      else {
+        document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+      }
+    }
+  });
 
 	// Track all titles that have an `id` applied
   document.querySelectorAll('h2[id]').forEach((section) => {
