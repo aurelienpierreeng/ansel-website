f61a3282d233129576292cd7cd8ab79749f6f4b7
Aurélien PIERRE <contact@aurelienpierre.com>
2023-03-18 18:27:12 +0100

    Sync style with community.ansel.photos

--- themes/ansel/static/js/scripts.js
+++ themes/ansel/static/js/scripts.js
@@ -27,7 +27,24 @@ window.addEventListener('DOMContentLoaded', () => {
 
 });
 
-// Create cookie to get the Browser Language
-const userLang = navigator.language || navigator.userLanguage;
-console.log(userLang);
-document.cookie = `nf_lang = ${userLang}`;
+
+document.addEventListener("DOMContentLoaded", function(){
+
+  el_autohide = document.querySelector('.autohide');
+
+  if(el_autohide){
+    var last_scroll_top = 0;
+    window.addEventListener('scroll', function() {
+          let scroll_top = window.scrollY;
+         if(scroll_top < last_scroll_top) {
+              el_autohide.classList.remove('scrolled-down');
+              el_autohide.classList.add('scrolled-up');
+          }
+          else {
+              el_autohide.classList.remove('scrolled-up');
+              el_autohide.classList.add('scrolled-down');
+          }
+          last_scroll_top = scroll_top;
+    });
+  }
+});
