b731e7e897bd5c8516a630fb1f3172b492e4da33
Aurélien PIERRE <contact@aurelienpierre.com>
2025-04-23 23:40:40 +0200

    Rename the last .en.md files

--- build-translations.sh
+++ build-translations.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# Get the documentation
+# Get the documentation module
 hugo mod get -u
 
-# Write it locally (=vendor)
+# Write it to disk locally (=vendor)
 hugo mod vendor
 
-# Auto-gen doc translations
+# Auto-gen doc translations from module disk cache
 sh _vendor/github.com/aurelienpierreeng/ansel-doc/tools/generate-translations.sh --no-update
+
+# Auto-gen website translations
+#sh tools/generate-translations.sh --no-update
